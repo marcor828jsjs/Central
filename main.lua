9dfb3e25336fb17ec203e927aa72f7fdbde90aca
marcor828jsjsj <yesanonimayo@gmail.com>
2025-05-15 14:14:02 -0300

    Script central detecta jogo e carrega script correto

---
+++ main.lua
@@ -0,0 +1,12 @@
+local games = {
+    [95508886069297] = "https://raw.githubusercontent.com/marcor828jsjs/Torre/main/torre.lua",
+    [83407947967527] = "https://raw.githubusercontent.com/marcor828jsjs/Slap/main/script.lua"
+}
+
+local script_url = games[game.PlaceId]
+
+if script_url then
+    loadstring(game:HttpGet(script_url))()
+else
+    warn("Jogo não suportado!")
+end
