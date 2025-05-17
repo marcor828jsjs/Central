13acece5e5d9ae9be9850f6fa19fba1832a015eb
marcor828jsjs <yesanonimayo@gmail.com>
2025-05-17 10:28:28 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 local games = {
     [95508886069297] = "https://raw.githubusercontent.com/marcor828jsjs/Torre/main/torre.lua",
     [83407947967527] = "https://raw.githubusercontent.com/marcor828jsjs/Slap/main/script.lua",
-    [79089892790758] = "https://raw.githubusercontent.com/marcor828jsjs/Torre2/main/script.lua"
+    [79089892790758] = "https://raw.githubusercontent.com/marcor828jsjs/Torre2/main/script.lua",
+    [15999929350] = "https://raw.githubusercontent.com/marcor828jsjs/Fixa-o1/main/Fixação1"
 }
 
 local script_url = games[game.PlaceId]
