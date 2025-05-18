44624f1dc321222bba2581121e50c97c1bd7177c
marcor828jsjs <yesanonimayo@gmail.com>
2025-05-18 15:11:11 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,9 @@ local games = {
     [83407947967527] = "https://raw.githubusercontent.com/marcor828jsjs/Slap/main/script.lua",
     [79089892790758] = "https://raw.githubusercontent.com/marcor828jsjs/Torre2/main/script.lua",
     [15999929350] = "https://raw.githubusercontent.com/marcor828jsjs/Fixa-o1/refs/heads/main/Fixa%C3%A7%C3%A3o1",
-    [105612566642310] = "https://raw.githubusercontent.com/marcor828jsjs/Torre3/refs/heads/main/Torre%20de%20slap%203"
+    [105612566642310] = "https://raw.githubusercontent.com/marcor828jsjs/Torre3/refs/heads/main/Torre%20de%20slap%203",
+    [83312952548612] = "https://raw.githubusercontent.com/marcor828jsjs/Troll2/refs/heads/main/Trolls%20na%20torre%20quebrada%202",
+    [128829279907343] = "https://raw.githubusercontent.com/marcor828jsjs/Torre-que-bate-trolls/refs/heads/main/Torre%20que%20bate%20trolls"
 }
 
 local script_url = games[game.PlaceId]
